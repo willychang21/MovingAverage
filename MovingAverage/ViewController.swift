4823c6a001c5d36d0764bbc5e5d225e191e36f80
willychang21 <willychang17@gmail.com>
2022-06-22 10:38:57 +0800

    Initial Commit

---
+++ MovingAverage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovingAverage
+//
+//  Created by 張竣傑 on 2022/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
